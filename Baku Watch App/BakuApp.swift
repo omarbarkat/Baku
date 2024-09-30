a172c0da50b0abbb15774a4c7fe3d43f2061a982
omar barkat <omarbarkat7@gmail.com>
2024-09-30 17:24:17 +0300

    Initial Commit

---
+++ Baku Watch App/BakuApp.swift
@@ -0,0 +1,17 @@
+//
+//  BakuApp.swift
+//  Baku Watch App
+//
+//  Created by eng.omar on 30/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Baku_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
